85b83f2f6ab9482e191c5f29a6e130ca88f4fbb8
Roman Johnson <125155563+stormcloud20@users.noreply.github.com>
2025-08-30 19:18:42 -0400

    Update card.py

--- card.py
+++ card.py
@@ -1,6 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker 
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+#pip install sqlalchemy
 
 
-class Card:
+#Replace USERNAME,PASSWORD, HOST, PORT, DBNAME with the info from config (or your own info)
+engine = create_engine('mysql+pymysql://USERNAME:PASSWORD@HOST:PORT/DBNAME')
+
+#Sessions allow you to interface with the DB from VS
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+class Card(Base):
+    _tablename_ = 'Cards'
+    card_id = Column(Integer, primary_key=True, nullable = False)
+    card_name = Column(String(50), nullable=False)
+    card_desc = Column(String(),nullable=False)
+    card_type = Column(String(), nullable=False)
     
     def __init__(self, name, desc, type):
         self.name = name
@@ -20,4 +39,6 @@ class Card:
 
     @property
     def type(self):
-        return self.type   
+        return self.type
+
+Base.metadata.create_all(engine)
